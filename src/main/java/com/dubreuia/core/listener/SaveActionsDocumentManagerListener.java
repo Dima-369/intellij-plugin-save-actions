67f026dc9642004498d6b05b3c06940ce3316d86
Dima Butemann <dbutemann@gmail.com>
2022-08-21 19:50:09 +0200

    Play around with trying to reformat any file in background for Emacs

--- src/main/java/com/dubreuia/core/listener/SaveActionsDocumentManagerListener.java
+++ src/main/java/com/dubreuia/core/listener/SaveActionsDocumentManagerListener.java
@@ -41,6 +41,34 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import java.util.Scanner;
+import com.intellij.ide.DataManager;
+import java.util.Collections;
+import com.intellij.openapi.actionSystem.DataConstants;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.fileEditor.FileEditor;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.AnAction;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import com.intellij.openapi.ui.playback.commands.ActionCommand;
+import com.intellij.util.concurrency.EdtExecutorService;
+import com.intellij.openapi.fileEditor.OpenFileDescriptor;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.actionSystem.ActionPlaces;
+import com.intellij.openapi.actionSystem.Presentation;
+import java.io.File;
+import com.intellij.psi.codeStyle.CodeStyleManager;
+import com.intellij.psi.PsiFile;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import com.intellij.psi.util.PsiUtilBase;
+
 import static com.dubreuia.core.ExecutionMode.saveAll;
 import static com.dubreuia.model.Action.activate;
 
@@ -54,9 +82,63 @@ public class SaveActionsDocumentManagerListener implements FileDocumentManagerLi
     private final Project project;
     private final PsiDocumentManager psiDocumentManager;
 
+    private static boolean setupEmacsChecker = false;
+
     public SaveActionsDocumentManagerListener(Project project) {
         this.project = project;
         psiDocumentManager = PsiDocumentManager.getInstance(project);
+
+        if (!setupEmacsChecker) {
+            setupEmacsChecker = true;
+            
+            Runnable toRun = new Runnable() {
+                    public void run() {
+                        File f = new File("/Users/Gira/emacs-intellij.txt");
+                        if (f.exists()) {
+
+                            String fileString = "";
+                            try {
+                                Path fileName = Path.of("/Users/Gira/emacs-intellij.txt");
+                                fileString = Files.readString(fileName);
+                            } catch (Exception e) {}
+                            
+                            f.delete();
+
+                            VirtualFile virtualFile = LocalFileSystem.getInstance().refreshAndFindFileByPath(fileString);
+                            FileEditorManager.getInstance(project).openTextEditor(
+                                                                                  new OpenFileDescriptor(project,
+                                                                                                         virtualFile
+                                                                                                         ),
+                                                                                  true // request focus to editor
+                                                                                  );
+                            
+                            ActionManager am = ActionManager.getInstance();
+                            final String id = "ReformatCode";
+                            final AnAction action = am.getAction(id);
+                            am.tryToExecute(action, ActionCommand.getInputEvent(id), null, ActionPlaces.UNKNOWN, true);
+
+                            String saveAllId = "SaveAll";
+                            final AnAction saveAllAction = am.getAction(saveAllId);
+                            am.tryToExecute(saveAllAction, ActionCommand.getInputEvent(saveAllId), null, ActionPlaces.UNKNOWN, true);
+                            
+                            am.tryToExecute(action, ActionCommand.getInputEvent(id), null, ActionPlaces.UNKNOWN, true);
+
+                            try {
+                                ProcessBuilder pb = new ProcessBuilder("/usr/local/bin/emacsclient", "-e", "(dima-emacs-intellij-done)");
+                            Process p = pb.start();
+                                p.waitFor();
+                            } catch (Exception e) {}
+
+                            File f3 = new File("/Users/Gira/emacs-intellij-test3.txt");
+                            try {
+                                f3.createNewFile();
+                            } catch (Exception e) {
+                            }
+                        }
+                    }
+                };
+            EdtExecutorService.getScheduledExecutorInstance().scheduleWithFixedDelay(toRun, 1, 1, TimeUnit.SECONDS);
+        }
     }
 
     @Override
